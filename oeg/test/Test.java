0720437a71e6f7daa0ef754bbf995b9673b0b0c3
joysaravana <joysaravana94@gmail.com>
2022-05-11 22:38:23 +0530

    chenged in B

--- oeg/test/Test.java
+++ oeg/test/Test.java
@@ -5,7 +5,8 @@ public class Test {
 	public static void main(String[] args) {
 		int a=10;
 		int b=10;
-		System.out.println("push and p");
+		System.out.println("push and pushcomment");
+		System.out.println("done");
 	}
 
 }
